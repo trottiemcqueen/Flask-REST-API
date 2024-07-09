671b7338ce4d5b279b5caa22431be23f537df727
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-07-09 18:20:51 -0400

    added instance after troubleshooting instance not showing

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
-from flask import flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 
-app = flask(__name__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
